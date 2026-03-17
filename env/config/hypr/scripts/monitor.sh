415e35720be914f0e5b9966f143be0ad76eede17
Leonardo de Abreu <leonardo.abreu@lia.ufc.br>
2026-03-16 21:50:08 -0300

    monitor resolution

--- env/config/hypr/scripts/monitor.sh
+++ env/config/hypr/scripts/monitor.sh
@@ -4,12 +4,12 @@ if [[ $(hyprctl monitors | grep -c 'HDMI-A-1') -eq 1 ]]; then
     hyprctl keyword monitor 'HDMI-A-1, 1920x1080@75, 0x0, 1'
     hyprctl keyword monitor 'eDP-1, disable'
 else
-    hyprctl keyword monitor 'eDP-1, 1366x768@60, 0x0, 1'
+    hyprctl keyword monitor 'eDP-1, 1920x1080@60, 0x0, 1'
 fi
 
 function handle {
 	if [[ "$USE_LAPTOP_MONITOR" -eq 1 ]]; then
-			hyprctl keyword monitor 'eDP-1, 1366x768@60, auto, 1'
+			hyprctl keyword monitor 'eDP-1, 1920x1080@60, auto, 1'
 	fi
 	if [[ ${1:0:12} == 'monitoradded' ]]; then
 		if [[ ${1:14:8} == 'HDMI-A-1' ]]; then
@@ -18,7 +18,7 @@ function handle {
 		fi
 	elif [[ ${1:0:14} == 'monitorremoved' ]]; then
 		if [[ ${1:16:8} == 'HDMI-A-1' ]]; then
-			hyprctl keyword monitor 'eDP-1, 1366x768@60, auto, 1'
+			hyprctl keyword monitor 'eDP-1, 1920x1080@60, auto, 1'
 		fi
 	elif [[ ${1:0:10} == 'focusedmon' ]]; then
 		if [[ ${1:12:8} == 'HDMI-A-1' ]]; then
